5633e83f1ff84808fafc2da09bc54f91d8332a35
Jack Zielinski <zielinskijr2830@aguafria.org>
2026-01-31 13:38:21 -0700

    creates a loop!

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
 import { renderDom } from './render.js';
 import { feed, play } from './actions.js'
-import { load } from './data.js'
+import { load, state } from './data.js'
 
 load()
 renderDom()
+
+const main_loop = setInterval(() => {
+    
+    if (state.time >= 24) {
+        state.time = 0;
+    } else {
+        state.time += 1;
+    }
+
+    renderDom()
+}, 1000);
