97f99b2bf2cbc9f4c8cd8093cc7f028e5b9260ce
Jack Zielinski <zielinskijr2830@aguafria.org>
2026-01-31 11:45:15 -0700

    adds some basic save and load stuffs

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+let state;
+
+const persist = localStorage.getItem("state");
+
+if (persist != null) {
+  state = JSON.parse(persist);
+} else {
+  state = {
+    time: 7,
+    hunger: 0,
+    sleep: 0,
+    mood: 10,
+    money: 100,
+    sick: false,
+    depressed: false,
+    in_bed: false,
+
+    type: null,
+    gender: null,
+    nickname: null
+  };
+}
+
+function feed() {
+  state.hunger += 1;
+  state.mood += 1;
+
+  localStorage.setItem("state", JSON.stringify(state));
+}
