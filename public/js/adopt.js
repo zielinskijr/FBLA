8bfebf7bdca016f3cc1ac6a2aba77cea6a56164c
Jack Zielinski <zielinskijr2830@aguafria.org>
2026-03-15 22:31:31 -0700

    readjusts the adopt page!

--- public/js/adopt.js
+++ public/js/adopt.js
@@ -1,12 +1,11 @@
 function saveInput() {
     const info = {
-        type: document.querySelector('input[name="type"]:checked')?.value,
-        nickname: document.querySelector('input[name="nickname"]')?.value,
-        gender: document.querySelector('input[name="gender"]:checked')?.value,
+        type: document.querySelector('select')?.value,
+        nickname: document.querySelector('input')?.value
     };
     localStorage.setItem("info", JSON.stringify(info));
 
-    if (info.type == null || info.nickname == "" || info.gender == null) {
+    if (info.type == null || info.nickname == "") {
         document.getElementById("error-text").removeAttribute("hidden");
     } else {
         window.location.href = "/";
